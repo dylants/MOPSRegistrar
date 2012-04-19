6053f2344bb6e0d9e583186212bee96a6a1845e0
Dylan Smith <dylantsmith@gmail.com>
2012-04-18 23:29:26 -0500

    add parameters to CryptUtilTest

--- src/test/java/com/mops/registrar/security/CryptUtilTest.java
+++ src/test/java/com/mops/registrar/security/CryptUtilTest.java
@@ -1,8 +1,14 @@
 package com.mops.registrar.security;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * {@link CryptUtil} tests
@@ -10,13 +16,25 @@ import org.junit.Test;
  * @author dylants
  * 
  */
+@RunWith(Parameterized.class)
 public class CryptUtilTest {
 
     private static final String TEST_MESSAGE = "mymessage";
     private static final String TEST_MESSAGE_WITH_SPACES = "this is my message";
     private static final String TEST_MESSAGE_WITH_SPECIAL_CHARS = "th1s i$ m* m3SS@~e";
 
+    private String message = null;
     private CryptUtil cryptUtil = null;
+
+    @Parameters
+    public static Collection<Object[]> buildData() {
+        return Arrays.asList(new Object[][] { { TEST_MESSAGE }, { TEST_MESSAGE_WITH_SPACES },
+                { TEST_MESSAGE_WITH_SPECIAL_CHARS } });
+    }
+
+    public CryptUtilTest(String message) {
+        this.message = message;
+    }
 
     @Before
     public void setupBefore() {
@@ -25,15 +43,15 @@ public class CryptUtilTest {
 
     @Test
     public void testEncodeDecode() {
-        String encodedMessage = this.cryptUtil.encode(TEST_MESSAGE);
+        String encodedMessage = this.cryptUtil.encode(this.message);
         String decodedMesesage = this.cryptUtil.decode(encodedMessage);
 
-        Assert.assertEquals("messages must match", TEST_MESSAGE, decodedMesesage);
+        Assert.assertEquals("messages must match", this.message, decodedMesesage);
     }
 
     @Test
     public void testEncodeDecodeEncode() {
-        String encodedMessage = this.cryptUtil.encode(TEST_MESSAGE);
+        String encodedMessage = this.cryptUtil.encode(this.message);
         String decodedMesesage = this.cryptUtil.decode(encodedMessage);
         String encodedBackMessage = this.cryptUtil.encode(decodedMesesage);
 
